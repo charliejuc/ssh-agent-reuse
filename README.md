382fea3b6b7512dca3d52863b60b215095eee236
charliejuc <charliejuc@gmail.com>
2019-12-24 17:20:44 +0100

    Creates or set ssh-agent when run ssh-agent-reuse.sh.
    new file:   README.md
    new file:   ssh-agent-reuse.sh

---
+++ README.md
@@ -0,0 +1,7 @@
+# SSH AGENT REUSE
+
+Script that allows the **ssh-agent** to be reused between terminals.
+
+You only need to run it inside **.bashrc, .zshrc or similar file**.
+
+Tested in Debian Strecth.
---
+++ ssh-agent-reuse.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set_agent_file=/tmp/set-ssh-agent.env
+agent_pid_file=/tmp/ssh-agent_.pid
+
+agent_data=''
+
+if [ ! -f $set_agent_file ] || [ ! -f $agent_pid_file ]; then
+	agent_data=`ssh-agent -s`
+	echo "$agent_data" | head -n 2 > $set_agent_file
+fi
+
+eval `cat $set_agent_file`
+
+if [ -f $agent_pid_file ]; then
+	agent_pid=`cat $agent_pid_file`
+
+	if ! kill -0 $agent_pid  &> /dev/null; then
+		agent_data=`ssh-agent -s`
+
+		eval $agent_data
+		echo "$agent_data" | head -n 2 > $set_agent_file
+		echo $SSH_AGENT_PID > $agent_pid_file
+	fi
+else
+	eval $agent_data
+	echo $SSH_AGENT_PID > $agent_pid_file
+fi
